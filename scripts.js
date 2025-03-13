864f583cc8d37c3dde6b7aeb868618f1d18058f5
eltonSSilv <elton.santos5@hotmail.com>
2025-03-13 18:43:01 -0300

    build a product area

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const showList = document.querySelector(".list-products");
+const modal = document.querySelector(".modal");
+const maskModal = document.querySelector(".modal-mask");
+
+function clickOnBtn() {
+  modal.style.left = "50%";
+  maskModal.style.visibility = "visible";
+}
+
+function toHideAdress() {
+  modal.style.left = "-30%";
+  maskModal.style.visibility = "hidden";
+}
